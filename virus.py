9765c0640f822a61a750b75dc10cbaae9c9075a5
theawesomeyopro <108738248+theawesomeyopro1@users.noreply.github.com>
2023-01-24 20:14:09 +1100

    Update virus.py

--- virus.py
+++ virus.py
@@ -1,29 +1,25 @@
-#Import Turtle
+
+from turtle import *
 import turtle
-
-tt=turtle.Turtle()
-turtle.bgcolor("gray")#Screen background color set to gray
-tt.pencolor("black")#Pencolor set to black
-
-tt.speed(0)
-tt.penup()
-tt.goto(0,200)#position of the turtle 
-tt.pendown()
-
-#Intialization of variables 
-forDis=0 
-dR=0
-
-while(True):
-    tt.forward(forDis)
-    tt.right(dR)
-    forDis+=3 # forDis increased by 3 on every iteration
-    dR+=1# right angle distance increased by 1 on every iteration 
-    #If the distance for right angle becomes 210 the loop breaks
-    if dR==210:
+tur = turtle.Turtle()
+ws =turtle.Screen()
+ws.title("Pythontpoint")
+turtle.screensize(800,400)
+ws.bgcolor("white")
+tur.pencolor("red")
+tur.pensize(4)
+Python=0
+Cpp=0
+tur.speed(0)
+tur.penup()
+tur.goto(0,260)
+tur.pendown()
+while True:
+    tur.forward(Python)
+    tur.right(Cpp)
+    Python+=3
+    Cpp+=1
+    if Cpp==210:
         break
-	#Hide the turtle on completion of loop
-    tt.hideturtle()
-
-#Completion of Turtle execution
+    tur.hideturtle()
 turtle.done()
